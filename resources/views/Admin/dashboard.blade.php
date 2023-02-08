c786465d86fcefb14a79ff6c2b6868ae76ab0b26
Leograzioli <leograzioli@gmail.com>
2023-02-08 20:57:22 +0100

    edit dashboard

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -1,23 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center mt-5">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container-fluid">
+        <div class="row justify-content-between vh-100">
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+            <div class="col-md-2 v-100 bg-info pt-5">
+                <nav>
+                    <ul>
+                        <li><a href="{{ route('admin.profiles.show', auth()->user()->id) }}"> Profile </a></li>
+                        <li><a href="{{ route('admin.sponsor') }}">sponsors</a></li>
+                        <li><a href="{{ route('admin.specialization') }}">Specialization</a></li>
+                        <li><a href="{{ route('admin.message') }}">message</a></li>
+                        <li><a href="{{ route('admin.feedback') }}">feedback</a></li>
+                    </ul>
+                </nav>
+            </div>
 
-                        <p>
-                            {{ __('You are logged in!') }}
-                        </p>
-                        
+
+
+            <div class="col-md-8 pt-5">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Dashboard') }}</div>
+    
+    
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+    
+                            <p>
+                                {{ __('You are logged in!') }}
+                            </p>
+    
+                        </div>
                     </div>
                 </div>
             </div>
