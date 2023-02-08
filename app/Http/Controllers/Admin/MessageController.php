4495bc7ef2cd28bad9983e91feb0cd7db3f39c46
Leograzioli <leograzioli@gmail.com>
2023-02-08 21:51:38 +0100

    add style to message

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
     public function index()
     {
-        return view('admin.messages.index');
+        $messages = Message::where('user_id', auth()->user()->id)->get();
+        return view('admin.messages.index', compact('messages'));
     }
 
     public function show()
--- resources/views/Admin/Messages/index.blade.php
+++ resources/views/Admin/Messages/index.blade.php
@@ -3,5 +3,14 @@
 @section('content')
     <div class="container mt-5">
         <h2>message</h2>
+
+        @foreach ($messages as $message)
+
+         <h2>{{$message->name}}</h2>
+         <p>{{ $message->text }}</p>
+         <a class="btn btn-success mt-2" href="{{ route('admin.message.show', $message->id) }} ">vedi messaggio</a>
+         <hr>
+            
+        @endforeach
     </div>
 @endsection
