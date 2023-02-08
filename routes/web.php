ea63202b8bca3d3d7e3abf65a10b3ebd1f2db6f9
Anatoliegit88 <anatolie.coroban88@gmail.com>
2023-02-08 13:14:04 +0100

    add first route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,19 +14,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function() {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+}); 
 require __DIR__.'/auth.php';
