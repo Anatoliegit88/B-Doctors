d6dcc986484c0e179a17cc467015b16e9965ec66
MyUsername <alberto.baglivi@gmail.com>
2023-02-09 16:15:00 +0100

    Add PW Control

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,27 +1,23 @@
-import './bootstrap';
-import '~resources/scss/app.scss';
-import * as bootstrap from 'bootstrap';
-import.meta.glob([
-    '../img/**'
-])
+import "./bootstrap";
+import "~resources/scss/app.scss";
+import * as bootstrap from "bootstrap";
+import.meta.glob(["../img/**"]);
 
 
-let pw = document.getElementById('password')
-let pwConfirm = document.getElementById('password-confirm')
-let submitBtn = document.getElementById('submit-registration')
-let registerForm = document.getElementById('register-form')
-let pwError = document.getElementById('pw-error')
-
-console.log(pw, pwConfirm, submitBtn, registerForm, pwError)
-
-registerForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+document.getElementById("submit-registration").addEventListener("click", function(evt) {
+ 
     
-    if(pw.value === pwConfirm.value) {
-        pwError.classList.add('d-none');
-        registerForm.submit()
 
-    }else {
-        pwError.classList.remove('d-none');
-    }
-})
+    let pw = document.getElementById("password");
+    let pwConfirm = document.getElementById("password-confirm");
+    let pwError = document.getElementById("pw-error");
+     
+    
+        if (pw.value === pwConfirm.value) {
+            pwError.classList.add("d-none");
+        } else {
+            pwError.classList.remove("d-none"); 
+            evt.preventDefault();
+        }
+
+  });
