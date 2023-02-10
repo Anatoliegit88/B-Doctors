24905a1370bcd1ee875f91e75c0e34edf650fe57
Leograzioli <leograzioli@gmail.com>
2023-02-10 11:11:24 +0100

    add photo and cv on profile controller

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use App\Models\UserDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ProfileController extends Controller
 {
@@ -27,23 +28,45 @@ class ProfileController extends Controller
     {
     
         $data = $request->all();
+        $data['slug'] = Str::slug($data['name'] . '-' . $data['surname']);
         $profile->update($data);
 
+        //controll ii on request has any file to upload
         if($request->hasFile('photo')){
             if ($profile->user_detail->photo){
                 Storage::delete($profile->user_detail->photo);
             }
             $img_path = Storage::put('images', $data['photo']); 
             $data['photo'] = $img_path;       
-        }
+        };
+
+        if($request->hasFile('curriculum')){
+            if ($profile->user_detail->curriculum){
+                Storage::delete($profile->user_detail->curriculum);
+            }
+            $cv_path = Storage::put('Curriculum', $data['curriculum']); 
+            $data['curriculum'] = $cv_path;       
+        };
 
         $profile->user_detail()->updateOrCreate([
             'phone' => $data['phone'],
             'performance' => $data['performance'],
-            'photo' => $data['photo'],
             'address' => $data['address'],
             'description' => $data['description'],
         ]);
+
+        if ($request->hasFile('photo')) {
+
+            $profile->user_detail()->update([
+                'photo' => $data['photo'],
+            ]);
+        }
+        if ($request->hasFile('curriculum')){
+
+            $profile->user_detail()->update([
+                'curriculum' => $data['curriculum']
+            ]);
+        }
 
         if ($request->has('specializations')) {
             $profile->specializations()->sync($request->specializations);
