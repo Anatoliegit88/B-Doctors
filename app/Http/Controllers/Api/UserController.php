bb2f07c2b1a4cb19f10dd233392e65e3b59bfe39
luishs0 <luishs.doc@gmail.com>
2023-02-13 10:18:36 +0100

    add feedback to api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::with('user_detail', 'specializations')->get();
+        $users = User::with('user_detail', 'specializations', 'feedbacks')->get();
 
         return response()->json([
             'success' => true,
