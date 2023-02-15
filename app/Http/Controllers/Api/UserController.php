f2fafe55ed801f61868cf53df7f366900c98c403
Leograzioli <leograzioli@gmail.com>
2023-02-16 00:11:53 +0100

    fix user controller api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -24,20 +24,9 @@ class UserController extends Controller
             $users = $users1->where('feedback_avg_vote', '>=', $vote)->where('feedback_count', '>', $request->feedback_num);
         } else if ($request->specialization_id && $request->vote) {
 
-<<<<<<< HEAD
-            $users = $users1->where('feedback_avg_vote', '>=', $vote)->where('feedback_count', '>', $request->feedback_num);
-
-
-        } else if ($request->specialization_id && $request->vote) {
             $users1 = User::whereHas('specializations', function ($q) use ($id) {
                 $q->where('id', $id);
             })->withAvg('feedback', 'vote')->with('user_detail', 'specializations', 'feedback')->get();
-
-=======
-            $users1 = User::whereHas('specializations', function ($q) use ($id) {
-                $q->where('id', $id);
-            })->withAvg('feedback', 'vote')->with('user_detail', 'specializations', 'feedback')->get();
->>>>>>> cebb9b2fb023febdb67f0ef1c0eeb9f35c8afe5b
 
             $users = $users1->where('feedback_avg_vote', '>=', $vote);
         } else if ($request->specialization_id && $request->feedback_num) {
@@ -49,43 +38,20 @@ class UserController extends Controller
             $users = $users1->where('feedback_count', '>', $request->feedback_num);
         } else if ($request->specialization_id) {
 
-<<<<<<< HEAD
-            $users = $users1->where('feedback_count', '>', $request->feedback_num);
-
-        } else if ($request->specialization_id) {
-
             $users = User::whereHas('specializations', function ($q) use ($id) {
                 $q->where('id', $id);
             })->withAvg('feedback', 'vote')->with('user_detail', 'specializations', 'feedback')->get();
-
-=======
-            $users = User::whereHas('specializations', function ($q) use ($id) {
-                $q->where('id', $id);
-            })->withAvg('feedback', 'vote')->with('user_detail', 'specializations', 'feedback')->get();
->>>>>>> cebb9b2fb023febdb67f0ef1c0eeb9f35c8afe5b
         } else if ($request->vote) {
 
             $users1 = User::with('user_detail', 'specializations', 'feedback')->withAvg('feedback', 'vote')->get();
             $users = $users1->where('feedback_avg_vote', '>=', $vote);
         } else if ($request->feedback_num) {
 
-<<<<<<< HEAD
-        } else if ($request->feedback_num) {
-
-            $users1 = User::with('user_detail', 'specializations', 'feedback')->withAvg('feedback', 'vote')->withCount('feedback')->get();
-            $users = $users1->where('feedback_count', '>', $request->feedback_num);
-
-        } else {
-
-            $users = User::with('user_detail', 'specializations', 'feedback')->withAvg('feedback', 'vote')->get();
-
-=======
             $users1 = User::with('user_detail', 'specializations', 'feedback')->withAvg('feedback', 'vote')->withCount('feedback')->get();
             $users = $users1->where('feedback_count', '>', $request->feedback_num);
         } else {
 
             $users = User::with('user_detail', 'specializations', 'feedback')->withAvg('feedback', 'vote')->get();
->>>>>>> cebb9b2fb023febdb67f0ef1c0eeb9f35c8afe5b
         }
 
         return response()->json([
