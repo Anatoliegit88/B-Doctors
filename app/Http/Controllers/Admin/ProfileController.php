58bd3843b576c541e79542a083e706795b519560
Leograzioli <leograzioli@gmail.com>
2023-02-16 14:29:23 +0100

    add validation to profile edit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -17,49 +17,58 @@ class ProfileController extends Controller
         return view('Admin.profiles.show', compact('profile'));
     }
 
-    public function edit()
+    public function edit(User $profile)
     {
         $user = User::with('user_detail', 'specializations')->where('id', auth()->user()->id)->get();
         $specializations = Specialization::all();
-        return view('Admin.profiles.edit', compact('user', 'specializations'));
+        return view('Admin.profiles.edit', compact('profile', 'specializations'));
     }
 
     public function update(Request $request, User $profile)
     {
-    
-       $request->validate([
-        'name' => ['required', 'string', 'max:255'],
-        'surname' => ['required' , 'nullable'],
-        'email' => ['required', 'string', 'email', 'max:255'],
-        'specialization' => ['required | min:1'],
-        'address' => ['required'],
-       ]); 
+
+        if (!$request->specializations) {
+            return redirect()->route('admin.profiles.edit', $profile->id)->with('message', 'you must choose at leats one specialization');
+        }
+
+        $data = $request->validate([
+            'name' => ['required', 'string', 'max:10'],
+            'surname' => ['required'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'address' => ['required'],
+            'phone' => ['nullable'],
+            'address' => ['required'],
+            'curriculum' => ['nullable'],
+            'photo' => ['nullable'],
+            'performance' => ['nullable'],
+            'description' => ['nullable'],
+            'specializations' => ['required', 'min:1']
+        ]);
 
 
 
-        $data = $request->all();
         $data['slug'] = Str::slug($data['name'] . '-' . $data['surname']);
         $profile->update($data);
 
         //controll ii on request has any file to upload
-        if($request->hasFile('photo')){
-            if ($profile->user_detail->photo){
+        if ($request->hasFile('photo')) {
+            if ($profile->user_detail->photo) {
                 Storage::delete($profile->user_detail->photo);
             }
-            $img_path = Storage::put('images', $data['photo']); 
-            $data['photo'] = $img_path; 
-            
+            $img_path = Storage::put('images', $data['photo']);
+            $data['photo'] = $img_path;
+
             $profile->user_detail()->update(['photo' => $data['photo']]);
         };
 
-        if($request->hasFile('curriculum')){
-            if ($profile->user_detail->curriculum){
+        if ($request->hasFile('curriculum')) {
+            if ($profile->user_detail->curriculum) {
                 Storage::delete($profile->user_detail->curriculum);
             }
-            $cv_path = Storage::put('Curriculum', $data['curriculum']); 
-            $data['curriculum'] = $cv_path; 
-            
-            $profile->user_detail()->update(['curriculum' => $data['curriculum'] ]);
+            $cv_path = Storage::put('Curriculum', $data['curriculum']);
+            $data['curriculum'] = $cv_path;
+
+            $profile->user_detail()->update(['curriculum' => $data['curriculum']]);
         };
 
         $profile->user_detail()->update([
