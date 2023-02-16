5dd2c0eb82453dff3cac4ddd04d6ba7b20b3ad8c
Leograzioli <leograzioli@gmail.com>
2023-02-16 13:25:56 +0100

    add validator on api messages and feedback

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -5,17 +5,38 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MessageController extends Controller
 {
-    public function store(Request $request) 
+    public function store(Request $request)
     {
         $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'name' => ['required'],
+            'email' => ['required', 'email', 'max:150'],
+            'text' => ['required'],
+            'user_id' => ['required']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        //save on db
         $message = new Message();
         $message->name = $data['name'];
         $message->email = $data['email'];
         $message->text = $data['text'];
-        $message->user_id = $data['user_id']; 
+        $message->user_id = $data['user_id'];
         $message->save();
+
+        return response()->json([
+            'success' => true
+        ]);
     }
 }
