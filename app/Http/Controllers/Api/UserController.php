208f144d0121cebacb28a2bc33c72e52d6cf3bac
Leograzioli <leograzioli@gmail.com>
2023-02-17 21:01:56 +0100

    fix query on user controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,6 +8,7 @@ use App\Models\Sponsor;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -18,30 +19,62 @@ class UserController extends Controller
         $id = $request->specialization_id;
         $vote = $request->vote;
 
-        $users = User::with('user_detail', 'specializations', 'feedback')
-        ->withCount('feedback')
-        ->withAvg('feedback', 'vote')
-        ->get();
-    
+        $sponsoredUser = User::leftJoin('sponsor_user', 'users.id', '=', 'sponsor_user.user_id')
+            ->whereDate('expiration_date', '>=', Carbon::now())
+            ->with('user_detail', 'specializations', 'feedback')
+            ->withCount('feedback')
+            ->withAvg('feedback', 'vote')
+            ->get();
+
+        $notSponsoredUser = User::leftJoin('sponsor_user', 'users.id', '=', 'sponsor_user.user_id')
+            ->where('expiration_date', '<', Carbon::now())
+            ->orWherenull('expiration_date')
+            ->with('user_detail', 'specializations', 'feedback')
+            ->withCount('feedback')
+            ->withAvg('feedback', 'vote')
+            ->orderBy(DB::raw('RAND()'))
+            ->get();
+
         if ($request->specialization_id) {
 
-            $users = User::whereHas('specializations', function ($q) use ($id) {
-                $q->where('id', $id);
-            })->withCount('feedback')->withAvg('feedback', 'vote')->with('user_detail', 'specializations', 'feedback')->get();
+            $sponsoredUser = User::leftJoin('sponsor_user', 'users.id', '=', 'sponsor_user.user_id')
+                ->whereDate('expiration_date', '>=', Carbon::now())
+                ->with('user_detail', 'specializations', 'feedback')
+                ->withCount('feedback')
+                ->withAvg('feedback', 'vote')
+                ->whereHas('specializations', function ($q) use ($id) {
+                    $q->where('id', $id);
+                })
+                ->get();
 
+                $notSponsoredUser = User::whereHas('specializations', function ($q) use ($id) {
+                    $q->where('id', $id);
+                })
+                ->leftJoin('sponsor_user', 'users.id', '=', 'sponsor_user.user_id')
+                ->where(function ($query) {
+                    $query->where('expiration_date', '<', Carbon::now())
+                        ->orWhereNull('expiration_date');
+                })
+                ->orderBy(DB::raw('RAND()'))
+                ->with('user_detail', 'specializations', 'feedback')
+                ->withCount('feedback')
+                ->withAvg('feedback', 'vote')
+                ->get();
         }
 
         if ($request->vote) {
- 
-            $users = $users->where('feedback_avg_vote', '>=', $vote);
 
+            $sponsoredUser = $sponsoredUser->where('feedback_avg_vote', '>=', $vote);
+            $notSponsoredUser = $notSponsoredUser->where('feedback_avg_vote', '>=', $vote);
         }
 
         if ($request->feedback_num) {
 
-            $users = $users->where('feedback_count', '>', $request->feedback_num);
-            
-        } 
+            $sponsoredUser = $sponsoredUser->where('feedback_count', '>', $request->feedback_num);
+            $notSponsoredUser = $notSponsoredUser->where('feedback_count', '>', $request->feedback_num);
+        }
+
+        $users = [...$sponsoredUser, ...$notSponsoredUser];
 
         return response()->json([
             'success' => true,
