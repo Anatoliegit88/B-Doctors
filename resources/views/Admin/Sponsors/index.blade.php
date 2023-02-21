4813fdabfa66a39d3104323d975ef399ede10b63
Baglio1683 <66316045+Baglio1683@users.noreply.github.com>
2023-02-21 15:51:37 +0100

    add layout sponsor

--- resources/views/Admin/Sponsors/index.blade.php
+++ resources/views/Admin/Sponsors/index.blade.php
@@ -17,16 +17,38 @@
         @endif
 
         @foreach ($sponsors as $sponsor)
-        <div>
-            <ul>
-                <li> Title: {{ $sponsor->title }}</li>
-                <li> Description: <br> {{ $sponsor->description }}</li>
-                <li> Price: {{ $sponsor->price }}</li>
-                <a class="btn btn-success mt-3" href="{{ route('admin.sponsor.show', $sponsor->id) }}">Buy</a>
-                <hr>
-            </ul>
-        </div>
+            @if ($sponsor->title == 'Silver')
+                <div class="bg-silv rounded d-flex justify-content-between align-items-center pt-3 pb-3 pe-3 mt-3">
+                    <ul class="no-margin">
+                        <li> Title: {{ $sponsor->title }}</li>
+                        <li> Description: <br> {{ $sponsor->description }}</li>
+                        <li> Price: {{ $sponsor->price }}</li>
+                    </ul>
+                    <a class="btn btn-success mt-3" href="{{ route('admin.sponsor.show', $sponsor->id) }}">Buy</a>
+                </div>
+            @endif
+            @if ($sponsor->title == 'Gold')
+                <div class="bg-gold rounded d-flex justify-content-between align-items-center pt-3 pb-3 pe-3 mt-3">
+                    <ul class="no-margin">
+                        <li> Title: {{ $sponsor->title }}</li>
+                        <li> Description: <br> {{ $sponsor->description }}</li>
+                        <li> Price: {{ $sponsor->price }}</li>
+                    </ul>
+                    <a class="btn btn-success mt-3" href="{{ route('admin.sponsor.show', $sponsor->id) }}">Buy</a>
+                </div>
+            @endif
+            @if ($sponsor->title == 'Platinum')
+                <div class="bg-platinum rounded d-flex justify-content-between align-items-center pt-3 pb-3 pe-3 mt-3">
+                    <ul class="no-margin">
+                        <li> Title: {{ $sponsor->title }}</li>
+                        <li> Description: <br> {{ $sponsor->description }}</li>
+                        <li> Price: {{ $sponsor->price }}</li>
+                    </ul>
+                    <a class="btn btn-success mt-3" href="{{ route('admin.sponsor.show', $sponsor->id) }}">Buy</a>
+                </div>
+            @endif
         @endforeach
+
 
     </div>
 @endsection
